6d2e2ba7c9acce628a6fc83fead5ff97877d278d
fandromeda <2825053613@qq.com>
2023-12-21 16:25:59 +0800

    my work on lab2_challenge1 is done

--- kernel/strap.c
+++ kernel/strap.c
@@ -50,6 +50,7 @@ void handle_mtimer_trap() {
   write_csr(sip, 0);
 }
 
+#include"memlayout.h"
 //
 // the page fault handler. added @lab2_3. parameters:
 // sepc: the pc when fault happens;
@@ -65,16 +66,19 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // virtual address that causes the page fault.
       // panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
       
-      // (stval < USER_STACK_TOP) {
+      if (stval < USER_STACK_TOP && stval >= USER_STACK_TOP - 20 * PGSIZE) {
         //page_walk(current->pagetable, stval, 1);
         {
           pagetable_t pa = (pagetable_t)alloc_page();
           map_pages(current->pagetable, ROUNDDOWN(stval, PGSIZE), PGSIZE, (uint64)pa, prot_to_type(PROT_WRITE | PROT_READ, 1));
           //sepc += 4;
-          break;
         }
-      //}
-      
+      }
+      else {
+        panic("this address is not available!"); // panic = exit -1
+        // write_csr(sepc, sepc + 4);
+      }
+      break;
     default:
       sprint("unknown page fault.\n");
       break;
@@ -97,7 +101,7 @@ void smode_trap_handler(void) {
   // if the cause of trap is syscall from user application.
   // read_csr() and CAUSE_USER_ECALL are macros defined in kernel/riscv.h
   uint64 cause = read_csr(scause);
-
+  // sprint("d %llu d \n",cause);
   // use switch-case instead of if-else, as there are many cases since lab2_3.
   switch (cause) {
     case CAUSE_USER_ECALL:

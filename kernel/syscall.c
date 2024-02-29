c312cfd1884beb515ae5c80c4b332b5667d6c85f
fandromeda <2825053613@qq.com>
2024-02-29 20:49:51 +0800

    add initial function

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -115,6 +115,12 @@ long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, l
       return sys_user_fork();
     case SYS_user_yield:
       return sys_user_yield();
+    case SYS_user_sem_new:
+      return sys_user_sem_new();
+    case SYS_user_sem_P:
+      return sys_user_sem_P();
+    case SYS_user_sem_V:
+      return sys_user_sem_V();
     default:
       panic("Unknown syscall %ld \n", a0);
   }

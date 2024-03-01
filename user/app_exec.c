944b796f41a66f848612845ea9a6beeaf082d7b3
fandromeda <2825053613@qq.com>
2024-03-01 23:07:25 +0800

    add some comment

--- user/app_exec.c
+++ user/app_exec.c
@@ -1,6 +1,7 @@
 #include "user_lib.h"
 #include "util/types.h"
-
+// to run this lab, we use "spike ./obj/riscv-pke ./bin/app_exec"
+// and we do not implement close all the open virtual file of process in function exec
 int main(int argc, char *argv[]) {
   printu("\n======== exec /bin/app_ls in app_exec ========\n");
   int ret = exec("/bin/app_ls");
